51ff89dd852c130740366e0d5152393bf10a31a1
Jadslog <121417622+Jadslog@users.noreply.github.com>
2023-03-20 11:20:49 -0600

    update main.js

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ YTLinks.forEach(link => {
         YTPlayerOverlay.classList.add("active");
         let videoLink = `https://www.youtube.com/embed/${link.dataset.link}`;
         YTPlayerPopup.src = videoLink; 
-    });
+      });
 });
 
 YTPlayerOverlay.addEventListener("click", () => {
-    YTPlayerOverlay.classList.remove("active");
+    YTPlayerOverlay.classList.remove("active");   
 });
